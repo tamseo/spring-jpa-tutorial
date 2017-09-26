4c37f74798cc9abbf9cd62eae783e323b698e422
Tam Nguyen <tamseo@gmail.com>
2017-09-26 17:53:47 +0900

    Spring JPA sample

---
+++ src/main/java/com/springjpa/repo/CustomerRepository.java
@@ -0,0 +1,10 @@
+package com.springjpa.repo;
+
+import com.springjpa.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+    List<Customer> findByLastName(String lastName);
+}
